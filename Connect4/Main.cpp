c2728ba84ed8ca569e8534e8e26023b88b4534c1
Jean-Christophe <jboisvert2016@gmail.com>
2017-11-29 14:40:18 -0500

    sfml

--- Connect4/Main.cpp
+++ Connect4/Main.cpp
@@ -1,5 +1,27 @@
-#include <SFML/Graphics.hpp>
-int main()
+#include<SFML\Graphics.hpp>
+#include <string>
+using namespace std;
+void main()
 {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "My window");
+	sf::Event event;
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+		{
+			// fermeture de la fenêtre lorsque l'utilisateur le souhaite
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
+		// effacement de la fenêtre en noir
+		window.clear(sf::Color::Black);
+
+		// c'est ici qu'on dessine tout
+		// window.draw(...);
+
+		// fin de la frame courante, affichage de tout ce qu'on a dessiné
+		window.display();
+	}
+	system("pause");
 }
